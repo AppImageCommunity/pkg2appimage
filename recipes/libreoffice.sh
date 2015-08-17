b587be646807adbc854a0265dc770db415fa2df6
probonopd <probono@puredarwin.org>
2015-08-17 23:51:53 +0200

    Update libreoffice.sh

--- recipes/libreoffice.sh
+++ recipes/libreoffice.sh
@@ -2,8 +2,6 @@
 
 set +e
 
-sudo apt-get install xorriso # TODO: Replace with something that does not need sudo
-
 VERSION=$(wget "http://www.libreoffice.org/download/libreoffice-fresh/" -O - | grep -o -e "/dl/src/.*/all/" | cut -d "/" -f 4 | head -n 1)
 OOODOWNLOADLINK="http://download.documentfoundation.org/libreoffice/stable/"$VERSION"/deb/x86_64/LibreOffice_"$VERSION"_Linux_x86-64_deb.tar.gz"
 mkdir -p ./ooo/ooo.AppDir
@@ -25,20 +23,37 @@ find -name *startcenter.png -path *hicolor*48x48* -exec cp \{\} . \;
 BINARY=$(cat *.desktop | grep "Exec=" | head -n 1 | cut -d "=" -f 2 | cut -d " " -f 1)
 
 # sed -i -e 's|/opt|../opt|g' ./usr/bin/$BINARY
-mkdir -p usr/bin/
 cd usr/bin/
 rm ./$BINARY
 find ../../opt -name soffice -path *program* -exec ln -s \{\} ./$BINARY \;
 cd ../../
 
 # (64-bit)
-wget -c "https://github.com/probonopd/AppImageKit/releases/download/1/AppRun"
+wget -c "https://downloads.sourceforge.net/project/portable/64bit/AppRun"
+# or (32-bit)
+wget -c "https://downloads.sourceforge.net/project/portable/AppRun"
+
 chmod a+x ./AppRun
+
+# Try to run ./AppRun
 
 cd ..
 
 # (64-bit)
-wget -c "https://github.com/probonopd/AppImageKit/releases/download/1/AppImageAssistant"
+wget -c "https://downloads.sourceforge.net/project/portable/64bit/AppImageAssistant%200.9.3-64bit"
+# or (32-bit)
+wget -c "http://downloads.sourceforge.net/project/portable/AppImageAssistant%200.9.3"
 
-xorriso -indev ./AppImageAssistant* -osirrox on -extract / ./AppImageAssistant.AppDir
-./AppImageAssistant.AppDir/package ./ooo.AppDir/ ooo.AppImage
+chmod a+x ./AppImageAssistant*
+./AppImageAssistant* ./ooo.AppDir/ ./ooo.AppImage
+
+ls -lh ./ooo.AppImage
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get -y install python-requests
+
+# Upload
+wget https://raw.githubusercontent.com/probonopd/travis2github/master/travis2github.py
+wget https://raw.githubusercontent.com/probonopd/travis2github/master/magic.py
+python travis2github.py ./ooo.AppImage
