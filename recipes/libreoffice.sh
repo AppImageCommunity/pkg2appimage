17a4ef35b27c0784497c854dd97b08d45c5e258b
probonopd <probono@puredarwin.org>
2015-08-17 23:53:26 +0200

    Update libreoffice.sh

--- recipes/libreoffice.sh
+++ recipes/libreoffice.sh
@@ -2,6 +2,8 @@
 
 set +e
 
+sudo apt-get install xorriso # TODO: Replace with something that does not need sudo
+
 VERSION=$(wget "http://www.libreoffice.org/download/libreoffice-fresh/" -O - | grep -o -e "/dl/src/.*/all/" | cut -d "/" -f 4 | head -n 1)
 OOODOWNLOADLINK="http://download.documentfoundation.org/libreoffice/stable/"$VERSION"/deb/x86_64/LibreOffice_"$VERSION"_Linux_x86-64_deb.tar.gz"
 mkdir -p ./ooo/ooo.AppDir
@@ -23,29 +25,23 @@ find -name *startcenter.png -path *hicolor*48x48* -exec cp \{\} . \;
 BINARY=$(cat *.desktop | grep "Exec=" | head -n 1 | cut -d "=" -f 2 | cut -d " " -f 1)
 
 # sed -i -e 's|/opt|../opt|g' ./usr/bin/$BINARY
+mkdir -p usr/bin/
 cd usr/bin/
 rm ./$BINARY
 find ../../opt -name soffice -path *program* -exec ln -s \{\} ./$BINARY \;
 cd ../../
 
 # (64-bit)
-wget -c "https://downloads.sourceforge.net/project/portable/64bit/AppRun"
-# or (32-bit)
-wget -c "https://downloads.sourceforge.net/project/portable/AppRun"
-
+wget -c "https://github.com/probonopd/AppImageKit/releases/download/1/AppRun"
 chmod a+x ./AppRun
-
-# Try to run ./AppRun
 
 cd ..
 
 # (64-bit)
-wget -c "https://downloads.sourceforge.net/project/portable/64bit/AppImageAssistant%200.9.3-64bit"
-# or (32-bit)
-wget -c "http://downloads.sourceforge.net/project/portable/AppImageAssistant%200.9.3"
+wget -c "https://github.com/probonopd/AppImageKit/releases/download/1/AppImageAssistant"
 
-chmod a+x ./AppImageAssistant*
-./AppImageAssistant* ./ooo.AppDir/ ./ooo.AppImage
+xorriso -indev ./AppImageAssistant* -osirrox on -extract / ./AppImageAssistant.AppDir
+./AppImageAssistant.AppDir/package ./ooo.AppDir/ ooo.AppImage
 
 ls -lh ./ooo.AppImage
 
