d0e4a99333e1da380bc7f97d999f73f8049ce8ad
probonopd <probono@puredarwin.org>
2015-08-22 00:12:25 +0200

    Create arduino.sh

---
+++ recipes/arduino.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+set +e
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get -y install python-requests xorriso # TODO: Replace with something that does not need sudo
+
+URL="http://downloads.arduino.cc/arduino-nightly-linux64.tar.xz"
+APP=Arduino
+mkdir -p ./$APP/$APP.AppDir
+cd ./$APP
+
+wget -c "$URL"
+
+tar xfv *.tar.xz
+mv ardu*/* $APP.AppDir
+
+cd $APP.AppDir/
+
+mv arduino AppRun
+cp lib/arduino_small.png arduino.png
+sed -i -e 's|FULL_PATH/||g' arduino.desktop
+
+cd ..
+
+xorriso -indev ./AppImageAssistant* -osirrox on -extract / ./AppImageAssistant.AppDir
+./AppImageAssistant.AppDir/package ./$APP.AppDir/ $APP.AppImage
+
+ls -lh ./$APP.AppImage
+
+# Upload
+cd ..
+wget https://raw.githubusercontent.com/probonopd/travis2github/master/travis2github.py
+wget https://raw.githubusercontent.com/probonopd/travis2github/master/magic.py
+python travis2github.py ./$APP.AppImage
