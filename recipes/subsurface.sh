64761acd107e8d12cfa5aac165a13860dae299e3
probonopd <probono@puredarwin.org>
2015-10-11 16:45:10 +0200

    Update subsurface.sh

--- recipes/subsurface.sh
+++ recipes/subsurface.sh
@@ -8,7 +8,7 @@ sudo apt-get update -qq
 sudo apt-get -y install python-requests xorriso # TODO: Replace with something that does not need sudo
 sudo apt-get -y install git g++ make autoconf libtool cmake pkg-config \
 libxml2-dev libxslt1-dev libzip-dev libsqlite3-dev \
-libusb-1.0-0-dev libgit2-dev \
+libusb-1.0-0-dev \
 qt5-default qt5-qmake qtchooser qttools5-dev-tools libqt5svg5-dev \
 libqt5webkit5-dev libqt5qml5 libqt5quick5 libqt5declarative5 \
 qtscript5-dev libssh2-1-dev libcurl4-openssl-dev qttools5-dev \
@@ -43,4 +43,4 @@ ls -lh ./$APP.AppImage
 cd ..
 wget https://raw.githubusercontent.com/probonopd/travis2github/master/travis2github.py
 wget https://raw.githubusercontent.com/probonopd/travis2github/master/magic.py
-python travis2github.py "$APP_$VERSION.AppImage
+python travis2github.py "$APP_$VERSION.AppImage"
