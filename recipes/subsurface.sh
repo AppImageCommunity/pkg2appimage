c00a7db91c99eb34a731246d9bff081943e4be87
probonopd <probono@puredarwin.org>
2015-10-11 20:50:44 +0200

    Update subsurface.sh

--- recipes/subsurface.sh
+++ recipes/subsurface.sh
@@ -13,14 +13,11 @@ libxml2-dev libxslt1-dev libzip-dev libsqlite3-dev libusb-1.0-0-dev libssh2-1-de
 wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
 tar xf cmake-3.2.2-Linux-x86_64.tar.gz
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_55/qt.55.gcc_64/5.5.0-2qt5_essentials.7z
-# wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_essentials.7z.sha1
-#wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0icu_53_1_ubuntu_11_10_64.7z
-#wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0icu_53_1_ubuntu_11_10_64.7z.sha1
+wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_55/qt.55.gcc_64/5.5.0-2icu-linux-g++-Rhel6.6-x64.7z
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_55/qt.55.gcc_64/5.5.0-2qt5_addons.7z
-#wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_addons.7z.sha1
-7z x 5.4.1-0qt5_essentials.7z > /dev/null
-7z x 5.4.1-0icu_53_1_ubuntu_11_10_64.7z > /dev/null
-7z x 5.4.1-0qt5_addons.7z > /dev/null
+7z x *_essentials.7z > /dev/null
+7z x *icu-linux-*.7z > /dev/null
+7z x *_addons.7z > /dev/null
 export PATH=$PWD/cmake-3.2.2-Linux-x86_64/bin/:$PWD/5.4/gcc_64/bin/:$PATH
 
 APP=Subsurface
