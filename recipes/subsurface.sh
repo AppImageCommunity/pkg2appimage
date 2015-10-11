e4fad36dd38c8a739acf09c215afb7d28e4221ee
probonopd <probono@puredarwin.org>
2015-10-11 20:03:02 +0200

    Update subsurface.sh

--- recipes/subsurface.sh
+++ recipes/subsurface.sh
@@ -25,10 +25,11 @@ wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_essentials.7z.sha1
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0icu_53_1_ubuntu_11_10_64.7z
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0icu_53_1_ubuntu_11_10_64.7z.sha1
+wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_addons.7z
+wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_addons.7z.sha1
 7z x 5.4.1-0qt5_essentials.7z > /dev/null
 7z x 5.4.1-0icu_53_1_ubuntu_11_10_64.7z > /dev/null
-
-ls
+7z x 5.4.1-0qt5_addons.7z > /dev/null
 export PATH=$PWD/cmake-3.2.2-Linux-x86_64/bin/:$PWD/5.4/gcc_64/bin/:$PATH
 
 #export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/:$PKG_CONFIG_PATH
