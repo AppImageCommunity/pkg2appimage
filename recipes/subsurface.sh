37c1224fd65f27da2df5346fddbad954761dc758
probonopd <probono@puredarwin.org>
2015-10-12 03:40:14 +0200

    Update subsurface.sh

--- recipes/subsurface.sh
+++ recipes/subsurface.sh
@@ -81,6 +81,7 @@ cp ../../5.5/gcc_64/lib/libQt5Sql.so.5 usr/lib
 cp ../../5.5/gcc_64/lib/libQt5Qml.so.5 usr/lib
 cp ../../5.5/gcc_64/lib/libQt5Quick.so.5 usr/lib
 cp ../../5.5/gcc_64/lib/libQt5OpenGL.so.5 usr/lib
+cp ../../5.5/gcc_64/lib/libicu* usr/lib
 cp -L /usr/lib/x86_64-linux-gnu/libssh2.so.1 usr/lib
 rm usr/lib/libstdc* usr/lib/libgobject* usr/lib/libX*
 cd -
