c58441084ec425aa285f5d8a5fe161951c2ef3d8
probonopd <probono@puredarwin.org>
2015-10-17 17:06:18 +0200

    [ci skip]

--- recipes/subsurface.sh
+++ recipes/subsurface.sh
@@ -10,9 +10,9 @@
 # Resulting AppImage is known to work on:
 # CentOS Linux 7 (Core) - CentOS-7.0-1406-x86_64-GnomeLive.iso
 # CentOS Linux release 7.1.1503 (Core) - CentOS-7-x86_64-LiveGNOME-1503.iso
+# Fedora 22 (Twenty Two) - Fedora-Live-Workstation-x86_64-22-3.iso
 # Ubuntu 15.04 (Vivid Vervet) - ubuntu-15.04-desktop-amd64.iso
 # Ubuntu 14.04.1 LTS (Trusty Tahr) - ubuntu-14.04.1-desktop-amd64.iso
-# Fedora 22 (Twenty Two) - Fedora-Live-Workstation-x86_64-22-3.iso
 # openSUSE Tumbleweed (20151012) - openSUSE-Tumbleweed-GNOME-Live-x86_64-Current.iso
 # Antergos - antergos-2014.08.07-x86_64.iso
 # elementary OS 0.3 Freya - elementary_OS_0.3_freya_amd64.iso
