95dd3c27c44ac1187c682ba2c8a15d84dc8ff933
probonopd <probono@puredarwin.org>
2015-11-06 23:50:41 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+# Test AppImage on many distributions
+# using Live CD ISO files
+
+git clone https://github.com/probonopd/AppImageKit.git
+cd AppImageKit
+
+sudo apt-get install fuse-unionfs
+cp /usr/bin/unionfs-fuse ./AppImageAssistant.AppDir/unionfs-fuse
+
+rm -r log
+sudo find /isodevice/boot/iso/*amd64*iso -exec bash ./AppImageAssistant.AppDir/testappimage {} /home/me/Downloads/Subsurface_4.5.1.109_x86_64.AppImage >> log 2>&1  \;
+sudo find /isodevice/boot/iso/*_64*iso -exec bash ./AppImageAssistant.AppDir/testappimage {} /home/me/Downloads/Subsurface_4.5.1.109_x86_64.AppImage >> log 2>&1  \;
+
+# Now have to close every Subsurface instance by hand
