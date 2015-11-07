7aaffde29c37cd50cd6f34b9669b7bf7e75c8dff
probonopd <probono@puredarwin.org>
2015-11-07 15:17:39 +0100

    Fix wrong path

--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@ sudo find /isodevice/boot/iso/*amd64*iso -exec bash ./AppImageAssistant.AppDir/t
 sudo find /isodevice/boot/iso/*_64*iso -exec bash ./AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_x86_64.AppImage >> log 2>&1  \;
 
 # Test 32-bit AppImage; works on a 64-bit host
-sudo find /isodevice/boot/iso/*i386*iso -exec linux32 ./AppImageAssistant.AppDir/testappimage /isodevice/boot/iso/ubuntu-12.04.2-desktop-i386.iso $HOME/Downloads/Subsurface_*_i386.AppImage >> log 2>&1  \;
-sudo find /isodevice/boot/iso/*i686*iso -exec linux32 ./AppImageAssistant.AppDir/testappimage /isodevice/boot/iso/ubuntu-12.04.2-desktop-i386.iso $HOME/Downloads/Subsurface_*_i386.AppImage >> log 2>&1  \;
+sudo find /isodevice/boot/iso/*i386*iso -exec linux32 ./AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_i386.AppImage >> log 2>&1  \;
+sudo find /isodevice/boot/iso/*i686*iso -exec linux32 ./AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_i386.AppImage >> log 2>&1  \;
 
 # Now have to close every Subsurface instance by hand
