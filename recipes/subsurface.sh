8e0977238915af5370352d9b21149514b43f5276
probonopd <probono@puredarwin.org>
2015-11-09 22:26:17 +0100

    apt-get --yes --force-yes so that it asks no further questions

--- recipes/subsurface.sh
+++ recipes/subsurface.sh
@@ -45,7 +45,7 @@ grep -r "main universe" /etc/apt/sources.list || sudo sed -i -e "s| main| main u
 
 # Install dependencies
 sudo apt-get update -q # Make sure universe is enabled
-sudo apt-get -y install python-requests p7zip-full pax-utils imagemagick  \
+sudo apt-get --yes --force-yes install python-requests p7zip-full pax-utils imagemagick  \
 git g++ make autoconf libtool pkg-config \
 libxml2-dev libxslt1-dev libzip-dev libsqlite3-dev libusb-1.0-0-dev libssh2-1-dev libcurl4-openssl-dev \
 mesa-common-dev libgl1-mesa-dev libgstreamer-plugins-base0.10-0 libxcomposite1 python-software-properties \
@@ -54,7 +54,7 @@ libfuse-dev libglib2.0-dev libc6-dev binutils fuse
 # Install newer gcc and g++ since cannot be compiled with the stock 4.6.3
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -q update
-sudo apt-get -q install g++-4.8 gcc-4.8
+sudo apt-get --yes --force-yes install g++-4.8 gcc-4.8
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 g++ --version
