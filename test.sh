c2793c74d5ed8db18a43280e80c1006989480719
probonopd <probono@puredarwin.org>
2015-11-15 19:03:33 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,18 +4,19 @@
 sudo apt-get -y install git unionfs-fuse
 
 git clone https://github.com/probonopd/AppImageKit.git
-cd AppImageKit
-
-cp /usr/bin/unionfs-fuse ./AppImageAssistant.AppDir/unionfs-fuse
+cp /usr/bin/unionfs-fuse ./AppImageKit/AppImageAssistant.AppDir/unionfs-fuse
 
 rm -r log
 
 # Test 64-bit AppImage
-sudo find /isodevice/boot/iso/*amd64*iso -exec bash ./AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_x86_64.AppImage >> log 2>&1  \;
-sudo find /isodevice/boot/iso/*_64*iso -exec bash ./AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_x86_64.AppImage >> log 2>&1  \;
+sudo find /isodevice/boot/iso/*amd64*iso -exec bash ./AppImageKit/AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_x86_64.AppImage >> log 2>&1  \;
+sudo find /isodevice/boot/iso/*_64*iso -exec bash ./AppImageKit/AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_x86_64.AppImage >> log 2>&1  \;
 
 # Test 32-bit AppImage; works on a 64-bit host
-sudo find /isodevice/boot/iso/*i386*iso -exec linux32 ./AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_i386.AppImage >> log 2>&1  \;
-sudo find /isodevice/boot/iso/*i686*iso -exec linux32 ./AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_i386.AppImage >> log 2>&1  \;
+sudo find /isodevice/boot/iso/*i386*iso -exec linux32 ./AppImageKit/AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_i386.AppImage >> log 2>&1  \;
+sudo find /isodevice/boot/iso/*i686*iso -exec linux32 ./AppImageKit/AppImageAssistant.AppDir/testappimage {} $HOME/Downloads/Subsurface_*_i386.AppImage >> log 2>&1  \;
 
 # Now have to close every Subsurface instance by hand
+
+# To get a summary
+grep -r SUCCESS log | cut -d " " -f 3 | cut -d "/" -f 5
