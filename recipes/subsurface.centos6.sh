599ebd52b3c5f635234ea27b930cd265358cc8e6
probonopd <probono@puredarwin.org>
2015-11-15 23:31:55 +0100

    Building on CentOS 6 Live ISO to make build products run on more systems

---
+++ recipes/subsurface.centos6.sh
@@ -0,0 +1,302 @@
+#!/bin/bash
+
+# Compile Subsurface and bundle it as an AppImage on CentOS 6
+# By Simon Peter 11/2015
+# This script is supposed to run inside a CentOS 6 environment. On a Ubuntu host machine, I used
+# the following commands set up a CentOS 6 environment; you can use your own chroot method of choice instead
+# or just boot the CentOS-6.7-$ARCH-LiveCD.iso in a virtual machine and run this script inside there.
+# sudo apt-get install unionfs-fuse
+# git clone https://github.com/probonopd/AppImageKit.git
+# cp /usr/bin/unionfs-fuse AppImageKit/AppImageAssistant.AppDir/
+# sudo linux32 ./AppImageKit/AppImageAssistant.AppDir/testappimage /isodevice/boot/iso/CentOS-6.7-i386-LiveCD.iso bash
+# or
+# sudo linux64 ./AppImageKit/AppImageAssistant.AppDir/testappimage /isodevice/boot/iso/CentOS-6.7-x86_64-LiveCD.iso bash
+# bash -ex ./build-inside-centos-6.sh
+
+# Resulting AppImage is known to run on
+# Chromixium-1.5-amd64.iso
+# elementary_OS_0.3_freya_amd64.iso
+# trisquel_7.0_amd64.iso
+# ubuntu-12.04.5-desktop-amd64.iso
+# ubuntu-14.04.1-desktop-amd64.iso
+# ubuntu-15.04-desktop-amd64.iso
+# ubuntu-15.10-desktop-amd64.iso
+# ubuntu-gnome-15.04-desktop-amd64.iso
+# ubuntu-gnome-15.10-desktop-amd64.iso
+# wily-desktop-amd64.iso
+# xubuntu-15.04-desktop-amd64.iso
+# CentOS-7.0-1406-x86_64-GnomeLive.iso
+# CentOS-7-x86_64-LiveGNOME-1503.iso
+# Fedora-Live-Desktop-x86_64-20-1.iso
+# Fedora-Live-Workstation-x86_64-22-3.iso
+# Fedora-Live-Workstation-x86_64-23-10.iso
+# ==
+# CentOS-6.4-i386-LiveDVD.iso
+# CentOS-6.5-i386-LiveCD.iso
+# CentOS-6.7-i386-LiveCD.iso
+# debian_7.0.0_wheezy_i386_20130705_binary.hybrid.iso
+# debian-live-7.5.0-i386-gnome-desktop.iso
+# Fedora-Live-Design-suite-i686-20-1.iso
+# Fedora-Live-Desktop-i686-19-1.iso
+# Fedora-Live-Desktop-i686-20-1.iso
+# Fedora-Live-Xfce-i686-20-1.iso
+# kali-linux-1.0.7-i386.iso
+# tails-i386-1.5.iso
+# ubuntu-11.04-desktop-i386.iso
+# ubuntu-12.04.2-desktop-i386.iso
+
+# Halt on errors
+set -e
+
+# Be verbose
+set -x
+
+# Determine which architecture should be built
+if [[ "$(arch)" = "i686" ||  "$(arch)" = "x86_64" ]] ; then
+	ARCH=$(arch)
+else
+	echo "Architecture could not be determined"
+	exit 1
+fi
+
+# Now we are inside CentOS 6
+grep -r "CentOS release 6" /etc/redhat-release || exit 1
+
+cd /root
+yum -y install git
+
+if [ ! -d AppImages ] ; then
+  git clone https://github.com/probonopd/AppImages.git
+fi
+cd AppImages/
+git stash save
+git pull --rebase
+cd ..
+
+# Enable EPEL repository; needed for recent Qt
+yum -y install epel-release
+
+# Install dependencies
+sudo yum -y install git make autoconf automake libtool \
+        libzip-devel libxml2-devel libxslt-devel libsqlite3x-devel \
+        libgit2-devel libudev-devel libusbx-devel libcurl-devel libssh2-devel mesa-libGL-devel sqlite-devel \
+        tar gzip which make autoconf automake gstreamer-devel ImageMagick
+
+# Need a newer gcc, getting it from Developer Toolset 2
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+yum -y install devtoolset-2-gcc devtoolset-2-gcc-c++ devtoolset-2-binutils
+# /opt/rh/devtoolset-2/root/usr/bin/gcc
+# now holds gcc and c++ 4.8.2
+
+# Get newer version of cmake
+# Install CMake 3.2.2 and Qt 5.5.x # https://github.com/vlc-qt/examples/blob/master/tools/ci/linux/install.sh
+if [[ "$ARCH" = "x86_64" ]] ; then
+	wget --no-check-certificate -c https://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
+fi
+if [[ "$ARCH" = "i686" ]] ; then
+	wget --no-check-certificate -c https://cmake.org/files/v3.2/cmake-3.2.2-Linux-i386.tar.gz
+fi
+tar xf cmake-*.tar.gz
+
+# EPEL is awesome - fresh Qt5 for old base systems
+yum -y install qt5-qtbase-devel qt5-qtlocation-devel qt5-qtscript-devel qt5-qtwebkit-devel qt5-qtsvg-devel qt5-linguist qt5-qtconnectivity-devel
+
+CMAKE_PATH=$(find $PWD/cmake-*/ -type d | head -n 1)bin
+export LD_LIBRARY_PATH=/opt/rh/devtoolset-2/root/usr/lib:$LD_LIBRARY_PATH # Needed for bundling the libraries into AppDir below
+export PATH=/opt/rh/devtoolset-2/root/usr/bin/:$CMAKE_PATH:$PATH # Needed at compile time to find Qt and cmake
+
+# Install AppImageKit build dependencies
+yum -y install binutils fuse glibc-devel glib2-devel fuse-devel gcc zlib-devel libpng12 # Fedora, RHEL, CentOS
+
+# Build AppImageKit
+if [ ! -d AppImageKit ] ; then
+  git clone https://github.com/probonopd/AppImageKit.git
+fi
+cd AppImageKit/
+git stash save
+git pull --rebase
+cmake .
+make clean
+make
+cd ..
+
+APP=Subsurface
+rm -rf ./$APP/$APP.AppDir
+mkdir -p ./$APP/$APP.AppDir
+cd ./$APP
+
+# Get latest subsurface project from git
+if [ ! -d subsurface ] ; then
+  git clone git://subsurface-divelog.org/subsurface
+fi
+cd subsurface/
+git stash save
+git pull --rebase
+cd ..
+
+# this is a bit hackish as the build.sh script isn't setup in
+# the best possible way for us
+mkdir -p $APP.AppDir/usr
+INSTALL_ROOT=$(cd $APP.AppDir/usr; pwd)
+sed -i "s,INSTALL_ROOT=.*,INSTALL_ROOT=$INSTALL_ROOT," ./subsurface/scripts/build.sh
+sed -i "s,cmake -DCMAKE_BUILD_TYPE=Debug.*,cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_ROOT .. \\\\," ./subsurface/scripts/build.sh
+bash -ex ./subsurface/scripts/build.sh
+( cd subsurface/build ; make install )
+
+cp ./subsurface/subsurface.desktop $APP.AppDir/
+cp ./subsurface/icons/subsurface-icon.png $APP.AppDir/
+mogrify -resize 64x64 $APP.AppDir/subsurface-icon.png
+
+# Bundle dependency libraries into the AppDir
+cd $APP.AppDir/
+cp ../../AppImageKit/AppRun .
+chmod a+x AppRun
+# FIXME: How to find out which subset of plugins is really needed? I used strace when running the binary
+mkdir -p ./usr/lib/qt5/plugins/
+
+if [ -e $(dirname /usr/li*/qt5/plugins/bearer) ] ; then
+  PLUGINS=$(dirname /usr/li*/qt5/plugins/bearer)
+else
+  PLUGINS=../../5.5/gc*/plugins/
+fi
+echo $PLUGINS # /usr/lib64/qt5/plugins if build system Qt is found
+cp -r $PLUGINS/bearer ./usr/lib/qt5/plugins/
+cp -r $PLUGINS/iconengines ./usr/lib/qt5/plugins/
+cp -r $PLUGINS/imageformats ./usr/lib/qt5/plugins/
+cp -r $PLUGINS/platforminputcontexts ./usr/lib/qt5/plugins/
+cp -r $PLUGINS/platforms ./usr/lib/qt5/plugins/
+cp -r $PLUGINS/platformthemes ./usr/lib/qt5/plugins/
+cp -r $PLUGINS/sensors ./usr/lib/qt5/plugins/
+cp -r $PLUGINS/xcbglintegrations ./usr/lib/qt5/plugins/
+
+if [ -e $(dirname /usr/li*/libicudata.so.42) ] ; then
+  LIB=$(dirname /usr/li*/libicudata.so.42)
+else
+  LIB=../../5.5/gc*/lib
+fi
+echo $LIB
+cp -a $LIB/libicu* usr/lib
+
+export LD_LIBRARY_PATH=./usr/lib/:../../5.5/gc*/lib/:$LD_LIBRARY_PATH
+ldd usr/bin/subsurface | grep "=>" | awk '{print $3}'  |  xargs -I '{}' cp -v '{}' ./usr/lib || true
+ldd usr/lib/qt5/plugins/platforms/libqxcb.so | grep "=>" | awk '{print $3}'  |  xargs -I '{}' cp -v '{}' ./usr/lib || true
+
+# The following are assumed to be part of the base system
+rm -f usr/lib/libcom_err.so.2 || true
+rm -f usr/lib/libcrypt.so.1 || true
+rm -f usr/lib/libdl.so.2 || true
+rm -f usr/lib/libexpat.so.1 || true
+rm -f usr/lib/libfontconfig.so.1 || true
+rm -f usr/lib/libgcc_s.so.1 || true
+rm -f usr/lib/libglib-2.0.so.0 || true
+rm -f usr/lib/libgpg-error.so.0 || true
+rm -f usr/lib/libgssapi_krb5.so.2 || true
+rm -f usr/lib/libgssapi.so.3 || true
+rm -f usr/lib/libhcrypto.so.4 || true
+rm -f usr/lib/libheimbase.so.1 || true
+rm -f usr/lib/libheimntlm.so.0 || true
+rm -f usr/lib/libhx509.so.5 || true
+rm -f usr/lib/libICE.so.6 || true
+rm -f usr/lib/libidn.so.11 || true
+rm -f usr/lib/libk5crypto.so.3 || true
+rm -f usr/lib/libkeyutils.so.1 || true
+rm -f usr/lib/libkrb5.so.26 || true
+rm -f usr/lib/libkrb5.so.3 || true
+rm -f usr/lib/libkrb5support.so.0 || true
+# rm -f usr/lib/liblber-2.4.so.2 || true # needed for debian wheezy
+# rm -f usr/lib/libldap_r-2.4.so.2 || true # needed for debian wheezy
+rm -f usr/lib/libm.so.6 || true
+rm -f usr/lib/libp11-kit.so.0 || true
+rm -f usr/lib/libpcre.so.3 || true
+rm -f usr/lib/libpthread.so.0 || true
+rm -f usr/lib/libresolv.so.2 || true
+rm -f usr/lib/libroken.so.18 || true
+rm -f usr/lib/librt.so.1 || true
+rm -f usr/lib/libsasl2.so.2 || true
+rm -f usr/lib/libSM.so.6 || true
+rm -f usr/lib/libusb-1.0.so.0 || true
+rm -f usr/lib/libuuid.so.1 || true
+rm -f usr/lib/libwind.so.0 || true
+rm -f usr/lib/libz.so.1 || true
+
+# These seem to be available on most systems but not Ubuntu 11.04
+# rm -f usr/lib/libffi.so.6 usr/lib/libGL.so.1 usr/lib/libglapi.so.0 usr/lib/libxcb.so.1 usr/lib/libxcb-glx.so.0 || true
+
+# Accoring to the Subsurface upstream project, these are not needed in the AppImage
+rm -f usr/lib/libdivecomputer.a || true
+rm -f usr/lib/libdivecomputer.la || true
+rm -f usr/lib/libdivecomputer.so || true
+rm -f usr/lib/libdivecomputer.so.0 || true
+rm -f usr/lib/libdivecomputer.so.0.0.0 || true
+rm -f usr/lib/libGrantlee_TextDocument.so || true
+rm -f usr/lib/libGrantlee_TextDocument.so.5.0.0 || true
+rm -f usr/lib/libssrfmarblewidget.so || true
+rm -f usr/lib/subsurface || true
+rm -f usr/bin/universal || true
+rm -f usr/bin/ostc-fwupdate || true
+rm -f usr/bin/subsurface.debug || true
+
+# Delete potentially dangerous libraries
+rm -f usr/lib/libstdc* usr/lib/libgobject* usr/lib/libc.so.* || true
+# Do NOT delete libX* because otherwise on Ubuntu 11.04:
+# loaded library "Xcursor" malloc.c:3096: sYSMALLOc: Assertion (...) Aborted
+
+# We don't bundle the developer stuff
+rm -rf usr/include || true
+rm -rf usr/lib/cmake || true
+rm -rf usr/lib/pkgconfig || true
+
+strip usr/bin/* usr/lib/* || true
+
+# According to http://www.grantlee.org/apidox/using_and_deploying.html
+# Grantlee looks for plugins in $QT_PLUGIN_DIR/grantlee/$grantleeversion/
+mv ./usr/lib/grantlee/ ./usr/lib/qt5/plugins/
+# Fix GDK_IS_PIXBUF errors on older distributions
+
+# TODO: Move all cp lib things to using ldconfig because it is more robust 
+# across different build system distributions and architectures.
+# It is the equivalent for "find" for libraries.
+cp $(ldconfig -p | grep libsasl2.so.2 | cut -d ">" -f 2 | xargs) ./usr/lib/
+cp $(ldconfig -p | grep libpng12.so.0 | cut -d ">" -f 2 | xargs) ./usr/lib/
+cp $(ldconfig -p | grep libGL.so.1 | cut -d ">" -f 2 | xargs) ./usr/lib/ # otherwise segfaults!?
+cp $(ldconfig -p | grep libGLU.so.1 | cut -d ">" -f 2 | xargs) ./usr/lib/ # otherwise segfaults!?
+# Fedora 23 seemed to be missing SOMETHING from the Centos 6.7. The only message was:
+# This application failed to start because it could not find or load the Qt platform plugin "xcb".
+# Setting export QT_DEBUG_PLUGINS=1 revealed the cause.
+# QLibraryPrivate::loadPlugin failed on "/usr/lib64/qt5/plugins/platforms/libqxcb.so" : 
+# "Cannot load library /usr/lib64/qt5/plugins/platforms/libqxcb.so: (/lib64/libEGL.so.1: undefined symbol: drmGetNodeTypeFromFd)"
+# Which means that we have to copy libEGL.so.1 in too
+cp $(ldconfig -p | grep libEGL.so.1 | cut -d ">" -f 2 | xargs) ./usr/lib/ # Otherwise F23 cannot load the Qt platform plugin "xcb"
+cp $(ldconfig -p | grep libxcb.so.1 | cut -d ">" -f 2 | xargs) ./usr/lib/ 
+
+# I have no clue why but on i386 systems this seems to be required
+if [[ "$ARCH" = "i686" ]] ; then
+	cp $(ldconfig -p | grep libgbm.so.1 | cut -d ">" -f 2 | xargs) ./usr/lib/ 
+fi
+
+cp $(ldconfig -p | grep libfreetype.so.6 | cut -d ">" -f 2 | xargs) ./usr/lib/ # For Fedora 20
+cd -
+find $APP.AppDir/
+
+# Figure out $VERSION
+GITVERSION=$(cd subsurface ; git describe | sed -e 's/-g.*$// ; s/^v//')
+GITREVISION=$(echo $GITVERSION | sed -e 's/.*-// ; s/.*\..*//')
+VERSION=$(echo $GITVERSION | sed -e 's/-/./')
+echo $VERSION
+
+if [[ "$ARCH" = "x86_64" ]] ; then
+	APPIMAGE=$PWD/$APP"_"$VERSION"_x86_64.AppImage"
+fi
+if [[ "$ARCH" = "i686" ]] ; then
+	APPIMAGE=$PWD/$APP"_"$VERSION"_i386.AppImage"
+fi
+
+# Put this script into the AppImage for debugging
+# FIXME: The follwong line does not work
+# cp $(readlink --canonicalize $0) ./$APP.AppDir/$APP.recipe
+
+# Convert the AppDir into an AppImage
+rm -rf $APPIMAGE
+../AppImageKit/AppImageAssistant.AppDir/package ./$APP.AppDir/ $APPIMAGE
+
+ls -lh $APPIMAGE
