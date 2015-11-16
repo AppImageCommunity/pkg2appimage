AppImageCommunity/pkg2appimage

commit d9e681369e502bbcd28c96fe4e57233a547ec414

Hopefully fix openSUSE Qt picking up the wrong libqxcb.so from the system

recipes/subsurface.centos6.sh
@@ -273,6 +273,12 @@ if [[ "$ARCH" = "i686" ]] ; then
 	cp $(ldconfig -p | grep libgbm.so.1 | cut -d ">" -f 2 | xargs) ./usr/lib/ 
 fi
 
+# On openSUSE Qt is picking up the wrong libqxcb.so
+# (the one from the system when in fact it should use the bundled one) - is this a Qt bug?
+# Hence, we binary patch /usr/lib* to $CWD/lib* which works because at runtime,
+# the current working directory is set to usr/ inside the AppImage before running the app
+cd usr/ ; find . -type f -exec sed -i -e 's|/usr/lib|././/lib|g' {} \; ; cd ..
+
 cp $(ldconfig -p | grep libfreetype.so.6 | cut -d ">" -f 2 | xargs) ./usr/lib/ # For Fedora 20
 cd -
 find $APP.AppDir/

commit 3b89c3af6fa5ebb4eda9fa650b6e3c425df63073

Fix issue of "cd -" returning to the wrong directory
I believe this was introduced in commit d9e681369e50 ("Hopefully fix openSUSE
Qt picking up the wrong libqxcb.so from the system") which changed the notion
of the previous directory at this point.

Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

recipes/subsurface.centos6.sh
@@ -280,7 +280,7 @@ fi
 cd usr/ ; find . -type f -exec sed -i -e 's|/usr/lib|././/lib|g' {} \; ; cd ..
 
 cp $(ldconfig -p | grep libfreetype.so.6 | cut -d ">" -f 2 | xargs) ./usr/lib/ # For Fedora 20
-cd -
+cd ..
 find $APP.AppDir/
 
 # Figure out $VERSION