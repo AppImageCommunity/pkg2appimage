6d84fd5559164daf8fe4c1e93c6206ec67f788a2
probonopd <probono@puredarwin.org>
2015-11-29 20:22:50 +0100

    Update scribus.centos6.sh

--- recipes/scribus.centos6.sh
+++ recipes/scribus.centos6.sh
@@ -30,12 +30,92 @@ rpm -ql boost-devel | grep 'cmake$' | xargs rm
 
 cmake . -DCMAKE_C_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++
 
+#####################
+exit 0
+#####################
+
+Boost Library Found OK
+Building without GraphicksMagick (use -DWANT_GRAPHICSMAGICK=1 to enable)
+-- checking for one of the modules 'libpoppler>=0.19.0;poppler>=0.19.0'
+Poppler NOT found - Disabling support for native PDF import
+-- checking for module 'librevenge-0.0'
+--   package 'librevenge-0.0' not found
+RPATH: lib/scribus/plugins/;
+-- Qt5::CoreQt5::WidgetsQt5::GuiQt5::XmlQt5::WebKitQt5::WebKitWidgetsQt5::NetworkQt5::OpenGL/usr/lib64/libxml2.so/usr/lib64/libz.so
+-- checking for module 'libwpg-0.2'
+--   package 'libwpg-0.2' not found
+-- checking for module 'libmspub-0.0=0.0'
+--   package 'libmspub-0.0=0.0' not found
+-- checking for module 'libwpg-0.2'
+--   package 'libwpg-0.2' not found
+-- Building with Scripter 1
+-- No source header files will be installed
+-- /scribus150/resources/translations
+-- The following GUI languages will be installed: 
+-- Configuring incomplete, errors occurred!
+See also "/scribus150/CMakeFiles/CMakeOutput.log".
+See also "/scribus150/CMakeFiles/CMakeError.log".
+[root@host scribus150]# gedit /scribus150/CMakeFiles/CMakeError.log
+
+(process:10244): Gtk-WARNING **: Locale not supported by C library.
+	Using the fallback 'C' locale.
+
+#####################
+
+cat /scribus150/CMakeFiles/CMakeError.logrror.log
+Determining if the system is big endian passed with the following output:
+Change Dir: /scribus150/CMakeFiles/CMakeTmp
+
+Run Build Command:/usr/bin/gmake "cmTryCompileExec62770080/fast"
+/usr/bin/gmake -f CMakeFiles/cmTryCompileExec62770080.dir/build.make CMakeFiles/cmTryCompileExec62770080.dir/build
+gmake[1]: Entering directory `/scribus150/CMakeFiles/CMakeTmp'
+/usr/bin/cmake -E cmake_progress_report /scribus150/CMakeFiles/CMakeTmp/CMakeFiles 1
+Building C object CMakeFiles/cmTryCompileExec62770080.dir/TestEndianess.c.o
+/opt/rh/devtoolset-2/root/usr/bin/gcc    -o CMakeFiles/cmTryCompileExec62770080.dir/TestEndianess.c.o   -c /scribus150/CMakeFiles/CMakeTmp/TestEndianess.c
+Linking C executable cmTryCompileExec62770080
+/usr/bin/cmake -E cmake_link_script CMakeFiles/cmTryCompileExec62770080.dir/link.txt --verbose=1
+/opt/rh/devtoolset-2/root/usr/bin/gcc       CMakeFiles/cmTryCompileExec62770080.dir/TestEndianess.c.o  -o cmTryCompileExec62770080 -rdynamic 
+gmake[1]: Leaving directory `/scribus150/CMakeFiles/CMakeTmp'
+
+TestEndianess.c:
+/* A 16 bit integer is required. */
+typedef unsigned short cmakeint16;
+
+/* On a little endian machine, these 16bit ints will give "THIS IS LITTLE ENDIAN."
+   On a big endian machine the characters will be exchanged pairwise. */
+const cmakeint16 info_little[] =  {0x4854, 0x5349, 0x4920, 0x2053, 0x494c, 0x5454, 0x454c, 0x4520, 0x444e, 0x4149, 0x2e4e, 0x0000};
+
+/* on a big endian machine, these 16bit ints will give "THIS IS BIG ENDIAN."
+   On a little endian machine the characters will be exchanged pairwise. */
+const cmakeint16 info_big[] =     {0x5448, 0x4953, 0x2049, 0x5320, 0x4249, 0x4720, 0x454e, 0x4449, 0x414e, 0x2e2e, 0x0000};
+
+#ifdef __CLASSIC_C__
+int main(argc, argv) int argc; char *argv[];
+#else
+int main(int argc, char *argv[])
+#endif
+{
+  int require = 0;
+  require += info_little[argc];
+  require += info_big[argc];
+  (void)argv;
+  return require;
+}
+
+#####################
+
+I cannot see any error...
+
+#####################
+
+
 # http://wiki.scribus.net/canvas/Librevenge
 # For those who always build the latest 1.5.0svn from source for testing, 
 # some new dependencies have been introduced to get the full functionality of 1.5.0.
 
 wget http://sourceforge.net/projects/libwpd/files/librevenge/librevenge-0.0.1/librevenge-0.0.1.tar.bz2/download
 tar xf librevenge-0.0.1.tar.bz2
+cd librevenge*
 ./autogen.sh
 
 # Autoconf version 2.65 or higher is required - we have 2.63. Yay!
