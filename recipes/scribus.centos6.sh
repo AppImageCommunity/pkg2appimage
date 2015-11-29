e7585c3d792c4ddc5bef003da7d5f8bed5d75672
probonopd <probono@puredarwin.org>
2015-11-30 00:27:06 +0100

    Update scribus.centos6.sh

--- recipes/scribus.centos6.sh
+++ recipes/scribus.centos6.sh
@@ -28,6 +28,8 @@ cd -
 
 # Workaround for: On CentOS 6, .pc files in /usr/lib/pkgconfig are not recognized
 # However, this is where .pc files get installed when bulding libraries... (FIXME)
+# I found this by comparing the output of librevenge's "make install" command
+# between Ubuntu and CentOS 6
 ln -sf /usr/share/pkgconfig /usr/lib/pkgconfig
 
 git clone git://git.code.sf.net/p/libwpd/librevenge librevenge
@@ -44,54 +46,61 @@ yum install libicu-devel
 git clone http://anongit.freedesktop.org/git/libreoffice/libmspub.git
 cd libmspub/
 ./autogen.sh
-./configure --prefix=/usr REVENGE_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_LIBS="-L/usr/lib/" REVENGE_STREAM_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_STREAM_LIBS="-L/usr/lib/" REVENGE_GENERATORS_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_GENERATORS_LIBS="-L/usr/lib/"
+./configure --prefix=/usr
 make
 make install
 cd -
-
-# Fails
-# undefined reference to `librevenge::RVNGFileStream::RVNGFileStream(char const*)'
 
 yum install gperf
 git clone http://anongit.freedesktop.org/git/libreoffice/libvisio.git
 cd libvisio/
 ./autogen.sh
-./configure --prefix=/usr REVENGE_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_LIBS="-L/usr/lib/" REVENGE_STREAM_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_STREAM_LIBS="-L/usr/lib/" REVENGE_GENERATORS_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_GENERATORS_LIBS="-L/usr/lib/"
+./configure --prefix=/usr
 make
 make install
 cd -
 
 # Fails
-# VSDXMLTokenMap.h:14:20: error: tokens.h: No such file or directory
+# VSDXMLTokenMap.h:14:20: error: tokens.h: No such file or directory ########################################
 
 git clone http://anongit.freedesktop.org/git/libreoffice/libcdr.git
 cd libcdr/
 ./autogen.sh
-./configure --prefix=/usr REVENGE_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_LIBS="-L/usr/lib/" REVENGE_STREAM_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_STREAM_LIBS="-L/usr/lib/" REVENGE_GENERATORS_CFLAGS="-I/usr/include/librevenge-0.0/" REVENGE_GENERATORS_LIBS="-L/usr/lib/"
+./configure --prefix=/usr
 make
 make install
 cd -
 
-# svn co svn://scribus.net/trunk/Scribus scribus15
-svn co -r 20099 svn://scribus.net/trunk/Scribus scribus150
+# yaml is needed for libpagemaker
+wget http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
+tar -xvzf yaml-0.1.4.tar.gz
+cd yaml-0.1.4
+./configure --prefix=/usr
+make
+make install
+cd -
+
+git clone https://github.com/umanwizard/libpagemaker.git
+cd libpagemaker/
+./autogen.sh
+./configure --prefix=/usr
+make
+make install
+cd -
+
+svn co svn://scribus.net/trunk/Scribus scribus15
+# svn co -r 20099 svn://scribus.net/trunk/Scribus scribus150
 
 cd scribus1*
 
 # Workaround for missing "/usr/lib64/lib64/libboost_date_time.a"
-ln -sf /usr/lib64/ /usr/lib64/lib64
+ls /usr/lib64/lib64 2>/dev/null || ln -sf /usr/lib64/ /usr/lib64/lib64
 
 # Workaround for missing "/usr/lib64/lib64/libboost_date_time-d.a"
 # http://comments.gmane.org/gmane.comp.mobile.osmocom.sdr/1097
 rpm -ql boost-devel | grep 'cmake$' | xargs rm
 
-## which gcc
-# /opt/rh/devtoolset-2/root/usr/bin/gcc
-# "Configuring incomplete, errors occurred!"
-## cat /scribus15/CMakeFiles/CMakeError.log | grep CXX
-# Compiling the CXX compiler identification source file "CMakeCXXCompilerId.cpp" failed.
-# Compiler: CMAKE_CXX_COMPILER-NOTFOUND 
-
+# Pass in -DCMAKE_C_COMPILER and -DCMAKE_CXX_COMPILER to prevent CMakeError.log CMAKE_CXX_COMPILER-NOTFOUND 
 cmake . -DCMAKE_C_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++
 
-
-
+# Does not find the libraries we compiled above; is this because the .so and -a files got installed into /usr/lib rather than /usr/lib64?
