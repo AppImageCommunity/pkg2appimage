afb5d9a51f7ee7455e4b13139eade4eb384c2473
probonopd <probono@puredarwin.org>
2015-11-29 20:05:22 +0100

    Update scribus.centos6.sh

--- recipes/scribus.centos6.sh
+++ recipes/scribus.centos6.sh
@@ -1,9 +1,13 @@
 # Enter a CentOS 6 chroot (you could use other methods)
 sudo ./AppImageKit/AppImageAssistant.AppDir/testappimage /isodevice/boot/iso/CentOS-6.5-x86_64-LiveCD.iso bash
 
-sudo yum -y install epel-release 
-
+yum -y install epel-release 
 yum -y install subversion cmake qt5-qtbase-gui qt5-qtbase qt5-qtbase-devel qt5-qtdeclarative qt5-qtdeclarative-devel qt5-qttools qt5-qttools-devel qt5-qtwebkit qt5-qtwebkit-devel qt5-qtbase-static glibc-headers libstdc++-devel gcc-c++ freetype-devel cairo-devel lcms2-devel libpng-devel libjpeg-devel libtiff-devel python-devel aspell-devel boost-devel cups-devel libxml2-devel libstdc++-devel boost-devel-static
+
+# Newer compiler than what comes with CentOS 6
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+yum -y install devtoolset-2-gcc devtoolset-2-gcc-c++ devtoolset-2-binutils
+bash /opt/rh/devtoolset-2/enable
 
 # svn co svn://scribus.net/trunk/Scribus scribus15
 svn co -r 20099 svn://scribus.net/trunk/Scribus scribus150
@@ -16,8 +20,6 @@ ln -sf /usr/lib64/ /usr/lib64/lib64
 # Workaround for missing "/usr/lib64/lib64/libboost_date_time-d.a"
 # http://comments.gmane.org/gmane.comp.mobile.osmocom.sdr/1097
 rpm -ql boost-devel | grep 'cmake$' | xargs rm
-
-bash /opt/rh/devtoolset-2/enable
 
 ## which gcc
 # /opt/rh/devtoolset-2/root/usr/bin/gcc
