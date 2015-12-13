d3f6d8102185d6a1f021cc2312ba8b0e08b7e1f0
probonopd <probono@puredarwin.org>
2015-12-13 03:28:54 +0100

    Update and rename build.sh to travis.sh
    [ci skip]

--- travis.sh
+++ travis.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Build on Travis CI with or without Docker
+
 set -e
+
+if [ ! $(env | grep TRAVIS_JOB_ID ) ] ; then
+  echo "This script is supposed to run on Travis CI"
+  exit 1
+fi
 
 RECIPE="${1}"
 
