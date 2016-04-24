ab71b44adb1a3d95f0f243006b949610cdc59091
probonopd <probonopd@users.noreply.github.com>
2016-04-24 09:32:39 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -43,3 +43,17 @@ copy_deps()
   done
   rm -f DEPSFILE
 }
+
+# Delete blacklisted files
+delete_blacklisted()
+{
+  BLACKLISTED_FILES=$(wget -q https://github.com/probonopd/AppImages/raw/master/excludelist -O - | sed '/^\s*$/d' | sed '/^#.*$/d')
+  echo $BLACKLISTED_FILES
+  for FILE in $BLACKLISTED_FILES ; do
+    FOUND=$(find . -type f -name "${FILE}" 2>/dev/null)
+    if [ ! -z "$FOUND" ] ; then
+      echo "Deleting blacklisted ${FOUND}"
+      rm -f "${FOUND}"
+    fi
+  done
+}
