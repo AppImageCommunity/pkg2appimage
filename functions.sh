5f713ac41abf4b5260b0e97e879a821a20693c12
probonopd <probonopd@users.noreply.github.com>
2016-05-15 10:54:33 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -86,3 +86,7 @@ generate_appimage()
   rm ../out/$APP"-"$VERSION"-x86_64.AppImage" || true
   ./AppImageAssistant ./$APP.AppDir/ ../out/$APP"-"$VERSION"-"$ARCH".AppImage"
 }
+
+# transfer.sh
+transfer() { if [ $# -eq 0 ]; then echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi 
+tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; } 
