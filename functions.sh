92cf6ccb044fa1d177db073974d4afb0a40983c1
probonopd <probonopd@users.noreply.github.com>
2016-09-18 23:21:18 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -160,6 +160,20 @@ generate_status()
   done
 }
 
+# Find the desktop file and copy it to the AppDir
+get_desktop()
+{
+  cp ./usr/share/applications/$LOWERAPP.desktop .
+}
+
+# Find the icon file and copy it to the AppDir
+get_icon()
+{
+  find ./usr/share/pixmaps/$LOWERAPP.png -exec cp {} . \; || true
+  find ./usr/share/icons -path *512* -name $LOWERAPP.png -exec cp {} . \; || true
+  find ./usr/share/icons -path *256* -name $LOWERAPP.png -exec cp {} . \; || true
+}
+
 # transfer.sh
 transfer() { if [ $# -eq 0 ]; then echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi 
 tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; } 
