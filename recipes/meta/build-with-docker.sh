ed7c4d84d3b67937d120ca55067cef0912422ac4
TheAssassin <theassassin@users.noreply.github.com>
2017-03-24 03:09:32 +0100

    Major improvements for Docker build system
      - Build in non-root containers (this means that a custom image has to be
        built for every user, but that shouldn't be a problem as only the user
        creation part creates a few new layers per image)
      - Avoid cache invalidations on recipe changes by mounting both Recipe and
        the .yml file as read only volumes
      - Add support for build.sh's caching
      - a few other minor improvements to build-with-docker.sh

--- recipes/meta/build-with-docker.sh
+++ recipes/meta/build-with-docker.sh
@@ -7,6 +7,10 @@ log() {
 }
 
 cleanup() {
+    if [ "$containerid" == "" ]; then
+        return 0
+    fi
+
     if [ "$1" == "error" ]; then
         log "error occurred, cleaning up..."
     elif [ "$1" != "" ]; then
@@ -23,33 +27,72 @@ trap "cleanup SIGTERM" SIGTERM
 trap "cleanup error" 0
 trap "cleanup" EXIT
 
-if [ "$1" == "" ]; then
-    log "Fatal error: no recipe name given!"
+CACHE=0
+RECIPE=""
+ARGS=""
+
+for var in $@; do
+    case "$1" in
+        -c|--cache)
+            if [ $CACHE -ne 0 ]; then
+                log "warning: caching already enabled"
+            fi
+            CACHE=1
+            ;;
+        *)
+            if [ "$RECIPE" != "" ]; then
+                log "warning: ignoring argument: $1"
+            else
+                RECIPE="$1"
+            fi
+            ;;
+    esac
+    shift
+done
+
+if [ "$RECIPE" == "" ]; then
+    log "usage: $0 [-c] name.yml"
+    log ""
+    log "\t-c, --cache\tEnable Recipe's caching by mounting the build cache directory into the container"
     exit 1
 fi
 
-recipe="$1"
-
-if [ $(basename $recipe .yml) == "$1" ]; then
-    recipe="$recipe.yml"
+if [ $(basename $RECIPE .yml) == "$RECIPE" ]; then
+    RECIPE="$RECIPE.yml"
 fi
 
-log  "Building $recipe in a container..."
+if [ ! -f $RECIPE ]; then
+    log "error: no such file or directory: $RECIPE"
+    exit 1
+fi
+
+if [ $CACHE -ne 0 ]; then
+    recipe_name=$(basename "$RECIPE" .yml)
+    mkdir -p "$recipe_name"
+    ARGS="-v $(readlink -f $recipe_name):/workspace/$recipe_name"
+fi
+
+log  "Building $RECIPE in a container..."
 
 randstr=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
 containerid=appimage-build-$randstr
-imageid=appimage-build
+imageid=appimage-build-$(id -u)
 
 log "Building Docker container"
-(set -x; docker build -t $imageid .)
+(set -xe; docker build -t $imageid --build-arg UID=$(id -u) --build-arg GID=$(id -g) .)
 
 log "Running container"
-set -x
+mkdir -p out
+set -xe
 docker run -it \
     --name $containerid \
     --cap-add SYS_ADMIN \
     --device /dev/fuse \
     --security-opt apparmor:unconfined \
-    -v $(readlink -f out)":/workspace/out" \
+    --user $(id -u):$(id -g) \
+    -v "$(readlink -f out):/workspace/out" \
+    -v "$(readlink -f Recipe):/workspace/Recipe:ro" \
+    -v "$(readlink -f $RECIPE):/workspace/$RECIPE:ro" \
+    $ARGS \
     $imageid \
-    ./Recipe $recipe
+    ./Recipe $RECIPE || cleanup error
