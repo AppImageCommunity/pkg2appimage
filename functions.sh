1a7ed5cdc8a673eff2669a80eb1000b932f2ef42
darealshinji <djcj@gmx.de>
2017-06-22 11:02:28 +0200

    Ignore blacklisted files in ./usr/optional/
    This makes it easier to customize the AppImage according to https://github.com/AppImage/AppImageKit/wiki/Creating-AppImages#libstdcso6

--- functions.sh
+++ functions.sh
@@ -84,7 +84,7 @@ delete_blacklisted()
   BLACKLISTED_FILES=$(cat_file_from_url https://github.com/AppImage/AppImages/raw/master/excludelist | sed 's|#.*||g')
   echo $BLACKLISTED_FILES
   for FILE in $BLACKLISTED_FILES ; do
-    FILES="$(find . -name "${FILE}")"
+    FILES="$(find . -name "${FILE}" -not -path "./usr/optional/*")"
     for FOUND in $FILES ; do
       rm -vf "$FOUND" "$(readlink -f "$FOUND")"
     done
