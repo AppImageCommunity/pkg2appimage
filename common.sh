88dfeafdc9d5fcd35f9c3e84fbd0cc86b12348db
Fernando Ortiz <fortiz2k@gmail.com>
2017-07-07 18:54:09 -0500

    make it more generic to allow other files types and also to work on Mac

---
+++ common.sh
@@ -0,0 +1,42 @@
+# return true if the input command exists in $PATH
+cmdExists() {
+  command -v $1 >/dev/null 2>/dev/null;
+  return $?;
+}
+
+getReadLink() {
+  for read_tool in 'greadlink' 'readlink'; do
+    cmdExists "$read_tool"
+    if [ $? = 0 -o -f "$read_tool" ]; then
+      READLINK="$read_tool"
+      return 0
+    fi
+  done
+  return 1
+}
+
+getSed() {
+  for sed_tool in 'gsed' 'sed'; do
+    cmdExists "$sed_tool"
+    if [ $? = 0 -o -f "$sed_tool" ]; then
+      SED="$sed_tool"
+      return 0
+    fi
+  done
+  return 1
+}
+
+getDate() {
+  for date_tool in 'gdate' 'date'; do
+    cmdExists "$date_tool"
+    if [ $? = 0 -o -f "$date_tool" ]; then
+      DATE="$date_tool"
+      return 0
+    fi
+  done
+  return 1
+}
+
+getReadLink
+getSed
+getDate
---
+++ dotenv.sh
@@ -0,0 +1,15 @@
+# .env loading in the shell
+dotenv () {
+  set -a
+  [ -f .env ] && . .env
+  set +a
+}
+
+# Run dotenv on login
+dotenv
+
+# Run dotenv on every new directory
+cd () {
+  builtin cd $@
+  dotenv
+}
