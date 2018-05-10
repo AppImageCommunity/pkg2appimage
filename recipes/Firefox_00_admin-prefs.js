53889bf307f072a8db31af5460474f0d020bc90e
probonopd <probonopd@users.noreply.github.com>
2018-05-10 10:59:27 +0000

    Create Firefox_00_admin-prefs.js

---
+++ recipes/Firefox_00_admin-prefs.js
@@ -0,0 +1,4 @@
+// Any comment. You must start the file with a single-line comment!
+// https://developer.mozilla.org/en-US/Firefox/Enterprise_deployment_before_60
+pref("general.config.filename", "mozilla.cfg");
+pref("general.config.obscure_value", 0);
